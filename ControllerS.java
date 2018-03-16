628f4cd9e8cc7fb16c61ebeaf7c596b4b0f331d3
Mirza Kurtalic <mkurtalic@student.tgm.ac.at>
2018-03-16 13:15:49 +0100

    final

---
+++ ControllerS.java
@@ -0,0 +1,29 @@
+package kurtalic;
+
+
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QDialog;
+
+public class ControllerS {
+	public Server server;
+	public Ui_ChatServer ui;
+
+	public ControllerS(Server server, Ui_ChatServer ui) {
+		this.server = server;
+		this.ui = ui;
+	}
+
+	public static void main(String[] args) {
+		Ui_ChatServer ui = new Ui_ChatServer();
+		Server server = new Server(5050, ui);
+		ServerRun run = new ServerRun(server, ui);
+		run.start();
+		QApplication.initialize(args);
+		QDialog d = new QDialog();
+		ui.setupUi(d);
+		d.show();
+		QApplication.execStatic();
+	}
+
+}
+
